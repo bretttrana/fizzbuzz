f441d5fdb3f3f67658667e576765021df96be28a
bretttrana <brett.trana@gmail.com>
2016-09-09 00:37:23 +0000

    One change from first commit: Changed fizz->Fizz, buzz->Buzz, and fizzbuzz->FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,13 +2,13 @@ function fizzbuzz (MAX) {
     for (var i = 1; i <= MAX; i++){
         var thing = i;
         if (i % 5 == 0) {
-            thing = 'buzz';
+            thing = 'Buzz';
         }
         if (i % 3 == 0) {
-            thing = 'fizz';
+            thing = 'Fizz';
         }
         if (i % 5 == 0 && i % 3 == 0) {
-            thing = 'fizzbuzz';
+            thing = 'FizzBuzz';
         }
         console.log(thing);
     }
